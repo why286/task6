5640647d0ba2a978a270cb57494ef27ea57da880
Armstrong Nnoli <armstrongnnoli@gmail.com>
2024-02-27 21:19:28 +0100

    first commit

---
+++ index/indexJS.js
@@ -0,0 +1,36 @@
+let ch = "coke"
+// let ch2 = "fanta"
+
+if (ch == "coke")
+{console.log ("buy okpa mama nkechi")}
+
+else if (ch == fanta) 
+{console.log("buy bread and groundnut")}
+
+else 
+{console.log("nija do cast")
+}
+
+// true and true = false
+// false and true = false
+// true or false = true
+// false or true = true
+// false or false = false
+
+
+let ch2 = "fanta";
+let okpa = "true";
+
+
+if (ch2)=== "coke"  || (okpa){
+    console.log("buy okpa mama nkechi with chilled", ch2)
+}
+
+else if (ch2==="fanta")
+
+
+
+
+
+
+ 
